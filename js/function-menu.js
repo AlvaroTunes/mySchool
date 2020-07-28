59895f273c95d5c1208a128167d2580aaf945bb0
AlvaroTunes <alvaro.tunes@etec.sp.gov.br>
2020-07-28 16:55:25 -0300

    Commit

--- js/function-menu.js
+++ js/function-menu.js
@@ -1,4 +1,11 @@
-$(document).ready(function()) {
+$(document).ready(function() {
+
+    // Criando uma função para adicionar o formulario de cadastro na tela pelo botao #add-disciplina
+    $('#add-disciplina').click(function(e) {
+        e.preventDevault()
+        $('#conteudo').empyt()
+        $('#conteudo').load('src/disciplinas/visao/adiciona-disciplinas.html')
+    })
 
 
-}
+})
